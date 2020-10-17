34c23b144735fac607c7b771384eb06b54dd461a
Rohan Bavishi <rbavishi@berkeley.edu>
2020-10-16 19:12:23 -0700

    Updating checker

--- gauss/domains/pandas_lite/checker.py
+++ gauss/domains/pandas_lite/checker.py
@@ -17,16 +17,21 @@ def is_placeholder(val, placeholders: List[str]):
     return isinstance(val, str) and any(val.startswith(i) or i in val for i in placeholders)
 
 
-def idx_match(t, r, phs: List[str]):
+def is_match(t, r, phs, strict: bool = False):
+    return (((not strict) and (is_placeholder(t, phs) or is_placeholder(r, phs))) or
+            t == r or (is_placeholder(t, phs) and is_placeholder(r, phs)))
+
+
+def idx_match(t, r, phs: List[str], strict: bool = False):
     try:
         if isinstance(t, tuple) and isinstance(r, tuple):
             #  Multi-index
             if len(t) != len(r):
                 return False
 
-            return all(is_placeholder(i, phs) or is_placeholder(j, phs) or i == j for i, j in zip(t, r))
+            return all(is_match(i, j, phs, strict=strict) for i, j in zip(t, r))
 
-        return is_placeholder(t, phs) or is_placeholder(r, phs) or t == r
+        return is_match(t, r, phs, strict=strict)
 
     except Exception as e:
         import logging
@@ -41,8 +46,12 @@ def get_col_mappings(target: pd.DataFrame, result: pd.DataFrame, phs: List[str])
 
     mappings = {}
     for idx, col in enumerate(target.columns):
+        #  If there are strict matchings, then only keep them.
         mappings[idx] = {r_idx for r_idx, r_val in enumerate(result.columns)
-                         if idx_match(col, r_val, phs)}
+                         if idx_match(col, r_val, phs, strict=True)}
+        if len(mappings[idx]) == 0:
+            mappings[idx] = {r_idx for r_idx, r_val in enumerate(result.columns)
+                             if idx_match(col, r_val, phs)}
 
     if any(len(i) == 0 for i in mappings.values()):
         raise UnequalException
@@ -58,9 +67,13 @@ def get_idx_mappings(target: pd.DataFrame, result: pd.DataFrame, phs: List[str],
         raise UnequalException
 
     mappings = {}
-    for idx, col in enumerate(target.index):
+    for idx, row in enumerate(target.index):
+        #  If there are strict matchings, then only keep them.
         mappings[idx] = {r_idx for r_idx, r_val in enumerate(result.index)
-                         if idx_match(col, r_val, phs) or ignore_row_ordering}
+                         if idx_match(row, r_val, phs, strict=True) or ignore_row_ordering}
+        if len(mappings[idx]) == 0:
+            mappings[idx] = {r_idx for r_idx, r_val in enumerate(result.index)
+                             if idx_match(row, r_val, phs) or ignore_row_ordering}
 
     if any(len(i) == 0 for i in mappings.values()):
         raise UnequalException
